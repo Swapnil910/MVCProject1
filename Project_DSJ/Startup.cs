0437a098516b69b55f7e7d617c75eba321410a89
swapn <swapn@DESKTOP-CP4CPF7>
2016-07-13 21:45:29 +0530

    Add project files.

---
+++ Project_DSJ/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_DSJ.Startup))]
+namespace Project_DSJ
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
